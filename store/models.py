08088ac4d0a90c4490179c7ed66562fa33bbbc44
Simon Paske <simas.paskevicius@gmail.com>
2023-09-05 13:18:32 +0000

    Add models and admin panel

--- store/models.py
+++ store/models.py
@@ -1,3 +1,73 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Customer(models.Model):
+    #
+    user = models.OneToOneField(
+        User, null=True, blank=True, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=50, null=True)
+    email = models.CharField(max_length=200, null=True)
+    address = models.CharField(max_length=200, null=True)
+    # null=True means that it can be blank
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    # image
+    description = models.TextField(max_length=200, null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=False)
+
+    def __str__(self):
+        return f"{self.name}: {self.rating}"
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, null=True)
+    feedback = models.TextField(max_length=200, null=True)
+    rating = models.IntegerField(default=0, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.product}: {self.rating}"
+class Order(models.Model):
+    # on_delete=models.SET_NULL will leave the order in the database even if the customer is deleted
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    order_date = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=True)
+    street_and_city = models.CharField(max_length=200, null=True)
+    country = models.CharField(max_length=200, null=True)
+    zipcode = models.IntegerField(null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
